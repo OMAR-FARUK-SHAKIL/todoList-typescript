f0437e9bac7ebde597d744c17deeb30cdb887c7d
OMAR-FARUK-SHAKIL <omarshakil8@gmail.com>
2021-09-04 01:13:49 +0600

    todo

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -26,7 +26,7 @@ const Header = () => {
 
     return (
         <div className="header">
-            <h1>This is heading</h1>
+            <h1>Track your tasks!!</h1>
 
             <input
                 onChange={handleChange}
